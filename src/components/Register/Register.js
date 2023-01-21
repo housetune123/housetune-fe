aba9f3a94a8418d3582b744cf9b64c9d28c80de6
Hikariyen881230 <hikariyen881230@gmail.com>
2023-01-21 22:02:28 +0800

    增加註冊功能

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,137 @@
 import React from 'react';
 import './Register.scss';
 import InputItem from './InputItem';
-import { Link } from 'react-router-dom';
+import { json, Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+
+function subcity(city) {
+  switch (city) {
+    case '台北市':
+      return [
+        '大安區',
+        '北投區',
+        '大同區',
+        '中山區',
+        '文山區',
+        '內湖區',
+        '士林區',
+        '松山區',
+        '萬華區',
+        '信義區',
+        '南港區',
+        '中正區',
+      ];
+    case '新北市':
+      return [
+        '萬里區',
+        '金山區',
+        '板橋區',
+        '汐止區',
+        '深坑區',
+        '石碇區',
+        '瑞芳區',
+        '平溪區',
+        '雙溪區',
+        '貢寮區',
+        '新店區',
+        '坪林區',
+        '烏來區',
+        '永和區',
+        '中和區',
+        '土城區',
+        '三峽區',
+        '樹林區',
+        '鶯歌區',
+        '三重區',
+        '新莊區',
+        '泰山區',
+        '林口區',
+        '蘆洲區',
+        '五股區',
+        '八里區',
+        '淡水區',
+        '三芝區',
+        '石門區',
+      ];
+    case '桃園市':
+      return [
+        '桃園區',
+        '中壢區',
+        '平鎮區',
+        '八德區',
+        '楊梅區',
+        '蘆竹區',
+        '大溪區',
+        '龜山區',
+        '大園區',
+        '觀音區',
+        '新屋區',
+        '龍潭區',
+        '復興區',
+      ];
+    default:
+      return [];
+  }
+}
 
 function Register() {
+  // let regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+  const [member, setMemeber] = useState({
+    account: '',
+    password: '',
+    rePassword: '',
+    name: '',
+    email: '',
+    phone: '',
+    address1: '',
+    address2: '',
+    address3: '',
+    bankcode: '',
+    bankaccount: '',
+  });
+
+  // const [errors, setErrors] = useState({
+  //   account: '',
+  //   password: '',
+  //   rePassword: '',
+  //   name: '',
+  //   email: '',
+  //   phone: '',
+  //   address1: '',
+  //   address2: '',
+  // });
+  axios.defaults.withCredentials = true;
+
+  function handleChange(e) {
+    setMemeber({ ...member, [e.target.name]: e.target.value });
+  }
+
+  function cityFunc(e) {
+    setCity(e.target.value);
+    handleChange(e);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (member.password !== member.rePassword) {
+      alert('密碼輸入不一致，請修正');
+      return;
+    }
+    try {
+      let response = await axios.post(
+        'http://localhost:3001/api/auth/register',
+        member
+      );
+      alert(response.data);
+      document.location.href = 'http://localhost:3000/login';
+    } catch (e) {
+      // console.log(e.response);
+      alert(e.response.data.errors[0]['msg']);
+    }
+  }
+  const [city, setCity] = useState('');
+
   return (
     <>
       <div className="pageBG bg-orange py-lg-5 py-4">
@@ -12,7 +140,12 @@ function Register() {
 
         {/* form */}
         <div className="container">
-          <form action="" method="" className="form-control">
+          <form
+            action=""
+            method=""
+            className="form-control"
+            onSubmit={handleSubmit}
+          >
             {/* form title */}
             <div className="mt-1 mb-4 py-2">
               <h1 className="text-center text-info fw-bold">加入會員</h1>
@@ -23,16 +156,22 @@ function Register() {
                 <div key={i} className="row mt-4 mt-lg-5">
                   <div className="col-4 d-none d-lg-block">
                     <h5 className="text-center text-info fw-bold">
-                      {it.tittle}
+                      {it.title}
                     </h5>
                   </div>
                   <div className="col col-lg-7">
                     <input
+                      required={it.req}
                       type={it.type}
                       className="form-control"
                       placeholder={it.preset}
+                      maxLength={it.max}
+                      minLength={it.min}
+                      onChange={handleChange}
+                      name={it.name}
                     ></input>
                   </div>
+                  {/* {errors[it.name]} */}
                 </div>
               );
             })}
@@ -43,13 +182,52 @@ function Register() {
               </div>
               <div className="col-7 d-flex justify-content-between">
                 <div className="d-none d-lg-block w-40">
-                  <select className="form-select">
-                    <option>新北市</option>
+                  <select
+                    className="form-select"
+                    onChange={cityFunc}
+                    name="address1"
+                    required
+                  >
+                    <option selected>請選擇城市</option>
+                    <option value="台北市">台北市</option>
+                    <option value="新北市">新北市</option>
+                    <option value="桃園市">桃園市</option>
+                    <option>台中市</option>
+                    <option>台南市</option>
+                    <option>高雄市</option>
+                    <option>新竹縣</option>
+                    <option>苗栗縣</option>
+                    <option>彰化縣</option>
+                    <option>南投縣</option>
+                    <option>雲林縣</option>
+                    <option>嘉義縣</option>
+                    <option>屏東縣</option>
+                    <option>宜蘭縣</option>
+                    <option>花蓮縣</option>
+                    <option>台東縣</option>
+                    <option>澎湖縣</option>
+                    <option>金門縣</option>
+                    <option>連江縣</option>
+                    <option>基隆市</option>
+                    <option>新竹市</option>
+                    <option>嘉義市</option>
                   </select>
                 </div>
                 <div className="d-none d-lg-block w-40">
-                  <select className="form-select">
-                    <option>新北市</option>
+                  <select
+                    className="form-select"
+                    name="address2"
+                    onChange={handleChange}
+                    required
+                  >
+                    <option selected>請選擇地區</option>
+                    {subcity(city).map((v, i) => {
+                      return (
+                        <option key={i} value={v}>
+                          {v}
+                        </option>
+                      );
+                    })}
                   </select>
                 </div>
               </div>
@@ -58,9 +236,12 @@ function Register() {
               <div className="col-4 d-none d-lg-block"></div>
               <div className="col-7 d-none d-lg-block">
                 <input
-                  type="email"
+                  type="text"
                   className="form-control"
-                  placeholder="請填入地址"
+                  placeholder="請輸入地址"
+                  name="address3"
+                  onChange={handleChange}
+                  required
                 ></input>
               </div>
             </div>
@@ -68,10 +249,15 @@ function Register() {
             {/* button */}
             <div className="d-flex d-lg-block">
               <div className="d-lg-flex justify-content-center mt-lg-5 mb-4">
-                <button className="btn btn-primary-300 me-2 me-lg-5 col-2">
+                <button
+                  className="btn btn-primary-300 me-2 me-lg-5 col-2"
+                  type="submit"
+                >
                   送出
                 </button>
-                <button className="btn btn-primary-300 col-2">清除</button>
+                <button type="reset" className="btn btn-primary-300 col-2">
+                  清除
+                </button>
               </div>
               {/* 登入Link */}
               <div className="d-flex mb-3 justify-content-center align-items-center">
